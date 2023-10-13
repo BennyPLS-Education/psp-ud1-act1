31455827f481fb0001a1f7a087bee34af1e8b3fc
Benny <peptoni0345@gmail.com>
2023-10-13 15:18:06 +0200

    Finished Child Procceses Act4

--- src/act4/ChildDir.java
+++ src/act4/ChildDir.java
@@ -5,44 +5,42 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * The ChildDir class is a utility that executes the ls/dir command on the user home.
+ * <p>
+ * On Linux, it uses the "ls".
+ * <p>
+ * On Windows, it uses the "dir".
+ */
 public class ChildDir {
     public static void main(String[] args) {
-        Process process = null;
-        try {
-            switch (System.getProperty("os.name")) {
-                case "Linux" -> process = new ProcessBuilder("ls", "-l", System.getProperty("user.home")).start();
-                case "Windows" -> process = new ProcessBuilder("dir", System.getProperty("user.home")).start();
-                default -> {
-                    System.out.println("Sistema operatiu no suportat");
-                    System.exit(1);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("No s'ha pogut crear el procés fill (Command 'dir')");
-            System.exit(1);
-        }
+        Process command = getProcess();
 
         try {
-            process.waitFor();
+            command.waitFor();
         } catch (InterruptedException e) {
             System.out.println("El procés fill ha estat interromput");
             System.exit(1);
         }
 
-        if (process.exitValue() != 0) {
+        if (command.exitValue() != 0) {
             System.out.println("El procés fill ha acabat amb un error");
             System.exit(1);
         }
 
-        String[] lines = new String[0];
+        final String[] lines = getProcessOutput(command);
 
-        try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            lines = reader.lines().toArray(String[]::new);
-        } catch (IOException e) {
-            System.out.println("No s'ha pogut llegir el procés fill");
-            System.exit(1);
-        }
+        writeFile(lines);
 
+        System.out.println("Fitxer creat");
+    }
+
+    /**
+     * Writes the given array of lines to a file named "output.txt".
+     *
+     * @param lines the array of lines to write to the file
+     */
+    private static void writeFile(String[] lines) {
         try (var writer = new FileWriter("output.txt")) {
             for (String line : lines) {
                 writer.write(line + "\n");
@@ -51,7 +49,66 @@ public class ChildDir {
             System.out.println("No s'ha pogut escriure al fitxer 'output.txt'");
             System.exit(1);
         }
+    }
 
-        System.out.println("Fitxer creat");
+    /**
+     * Retrieves the output of a given process as an array of strings.
+     *
+     * @param command The process to get the output from.
+     * @return An array of strings representing the output lines of the process.
+     */
+    private static String[] getProcessOutput(Process command) {
+        String[] lines = null;
+
+        try (var reader = new BufferedReader(new InputStreamReader(command.getInputStream()))) {
+            lines = reader.lines().toArray(String[]::new);
+        } catch (IOException e) {
+            System.out.println("No s'ha pogut llegir el procés fill");
+            System.exit(1);
+        }
+
+        return lines;
+    }
+
+    /**
+     * Retrieves a new process object from the getProcessBuilder method.
+     *
+     * @return The process object.
+     */
+    private static Process getProcess() {
+        ProcessBuilder pb = getProcessBuilder();
+        Process process = null;
+
+        try {
+            process = pb.start();
+        } catch (IOException e) {
+            System.out.println("No s'ha pogut crear el procés fill");
+            System.exit(1);
+        }
+
+        return process;
+    }
+
+    /**
+     * Returns a ProcessBuilder object configured based on the operating system.
+     *
+     * @return A ProcessBuilder object configured with the appropriate command based on the operating system.
+     */
+    private static ProcessBuilder getProcessBuilder() {
+        ProcessBuilder pb = null;
+        String os = System.getProperty("os.name").toLowerCase();
+        String userHome = System.getProperty("user.home");
+
+
+        switch (os) {
+            case "linux" -> pb = new ProcessBuilder("ls", "-l", userHome);
+            case "windows" -> pb = new ProcessBuilder("dir", userHome);
+            default -> {
+                System.out.println("Sistema operatiu no suportat");
+                System.exit(1);
+            }
+        }
+
+        return pb;
     }
 }
--- src/act4/ChildFind.java
+++ src/act4/ChildFind.java
@@ -1,61 +1,109 @@
 package act4;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class ChildFind {
+/**
+ * The ChildFind class is a utility that searches for a specified text in a file specified by the user.
+ * <p>
+ * The text and file path are provided as command-line arguments. The utility works differently based on the operating system.
+ * <p>
+ * On Linux, it uses the "grep" command with the case-insensitive option to search for the text in the file.
+ * <p>
+ * On Windows, it uses the "find" command with the case-insensitive option to search for the text in the file.
+ */
+class ChildFind {
     public static void main(String[] args) {
+        String path = getPath(args);
+        String text = getText();
+
+        Process command = getProcess(text, path);
+
+        try {
+            command.waitFor();
+        } catch (InterruptedException e) {
+            System.out.println("El procés fill ha estat interromput");
+        }
+
+        if (command.exitValue() != 0) {
+            System.out.println("El procés fill ha acabat amb un error");
+            System.exit(1);
+        }
+
+        try {
+            command.getInputStream().transferTo(System.out);
+        } catch (IOException e) {
+            System.out.println("No s'ha pogut llegir el procés fill");
+        }
+    }
+
+    /**
+     * Reads a line of text from the standard input using the Scanner class.
+     *
+     * @return the line of text read from the standard input
+     */
+    private static String getText() {
+        try (var scanner = new Scanner(System.in)) {
+            return scanner.nextLine();
+        }
+    }
+
+    /**
+     * Returns the path from the command line arguments.
+     *
+     * @param args the command line arguments
+     * @return the path represented as a string
+     */
+    private static String getPath(String[] args) {
         if (args.length != 1) {
             System.out.println("S'ha d'introduir un argument");
             System.exit(1);
         }
-        var path = args[0];
 
-        ProcessBuilder pb = getProcessBuilder(path);
+        return args[0];
+    }
 
-        Process command = null;
+    /**
+     * Returns a Process object created from the given text and path using the getProcessBuilder method.
+     *
+     * @param text the text to be processed
+     * @param path the path used for the process
+     * @return The created process object.
+     */
+    private static Process getProcess(String text, String path) {
+        ProcessBuilder pb = getProcessBuilder(text, path);
+        Process process = null;
+
         try {
-            command = pb.start();
+            process = pb.start();
         } catch (IOException e) {
             System.out.println("No s'ha pogut crear el procés fill");
             System.exit(1);
         }
 
-        var in = new BufferedReader(new InputStreamReader(command.getInputStream()));
-
-        String line = null;
-        while (true) {
-            try {
-                if ((line = in.readLine()) == null) break;
-            } catch (IOException e) {
-                System.out.println("No s'ha pogut llegir el procés fill");
-            }
-            System.out.println(line);
-        }
+        return process;
     }
 
-    private static ProcessBuilder getProcessBuilder(String path) {
-        String text;
+    /**
+     * Returns a ProcessBuilder object configured based on the given text and path.
+     *
+     * @param text the text to be searched
+     * @param path the path to search in
+     * @return A ProcessBuilder object configured with the appropriate command based on the operating system.
+     */
+    private static ProcessBuilder getProcessBuilder(String text, String path) {
+        ProcessBuilder pb = null;
+        String os = System.getProperty("os.name").toLowerCase();
 
-        try (var scanner = new Scanner(System.in)) {
-            text = scanner.nextLine();
-        }
-
-        switch (System.getProperty("os.name").toLowerCase()) {
-            case "linux" -> {
-                return new ProcessBuilder("grep", "-i", text, path);
-            }
-            case "windows" -> {
-                return new ProcessBuilder("find", "/i", text, path);
-            }
+        switch (os) {
+            case "linux" -> pb = new ProcessBuilder("grep", "-i", text, path);
+            case "windows" -> pb = new ProcessBuilder("find", "/i", text, path);
             default -> {
                 System.out.println("Sistema operatiu no suportat");
                 System.exit(1);
             }
         }
 
-        return null;
+        return pb;
     }
 }
